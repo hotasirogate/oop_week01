e996b7f140d4fca0da6c87f6844844e54e75d49f
unknown <hotasimanurung28@gmail.com>
2026-02-04 10:53:26 +0700

    week01: initial setup and procedural

---
+++ src/oop_00000077278_HotasiRogateManurung_week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000077278_HotasiRogateManurung_week01
+
+fun main() {
+    var name: String = "John Thor"
+    var Score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + Score)
+}
