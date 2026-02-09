0e6a356f0ac7f4c168178fedd2a56bb0cc5f0f9f
unknown <hotasimanurung28@gmail.com>
2026-02-09 08:46:48 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000077278_HotasiRogateManurung_week01/CircleCalculator.kt
+++ src/oop_00000077278_HotasiRogateManurung_week01/CircleCalculator.kt
@@ -1,13 +1,12 @@
 package oop_00000077278_HotasiRogateManurung_week01
 
-fun main(args: Array<String>) {
+fun main() {
 
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
-
     println(checkSize(area))
 }
 
