64995e5b92a08efcd6d0eece7c716f52955839a2
unknown <hotasimanurung28@gmail.com>
2026-02-09 08:56:12 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000077278_HotasiRogateManurung_week01/GameStore.kt
+++ src/oop_00000077278_HotasiRogateManurung_week01/GameStore.kt
@@ -1,4 +1,19 @@
 package oop_00000077278_HotasiRogateManurung_week01
 
-class GameStore {
+fun main() {
+
+    val gameTitle = "Minecraft"
+    val price = 600000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    println("Game: $gameTitle")
+    println("Price: Rp $price")
+    println("Discount: Rp $discount")
+    println("Final Price: Rp $finalPrice")
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) (price * 20) / 100
+    else (price * 10) / 100
